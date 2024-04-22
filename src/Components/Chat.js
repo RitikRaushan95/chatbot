f3258b13cbf4bcf95f31d5303afe1f555976a908
Ritik <ritikraushan160@gmail.com>
2024-04-22 18:58:35 +0530

    commit

---
+++ src/Components/Chat.js
@@ -0,0 +1,77 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const Chat = () => {
+  const [question, setQuestion] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [chatResponse,setChatResponse]=useState('')
+
+  const sendMessage = () => {
+    if (inputValue.trim() === '') return;
+    setQuestion(inputValue)
+    gemini();
+  };
+  
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+
+  const gemini = async () => {
+    try {
+      const response = await axios({
+        url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyC9IhchGDyUjcrIItScgnqURrs0W5B9gBg`,
+        method: "post",
+        data: {"contents":[{"parts":[{"text":inputValue}]}]},
+      });
+      const mybot= await response.data?.candidates[0]?.content?.parts[0].text;
+      setChatResponse(mybot);
+      console.log(mybot)
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  
+ 
+
+  return (
+   <>
+   <div className="flex h-screen">
+  <div className="flex flex-col w-full justify-center">
+    <div className="flex-1 overflow-y-auto p-4">
+    <div className="flex justify-end w-full">
+        <div className="bg-gray-200 text-gray-800 p-3 rounded-lg">
+          {question}
+        </div>
+    </div>
+
+
+      <div className="flex justify-start w-full">
+        <div className="bg-blue-200 text-gray-800 p-3 rounded-lg max-w-1/2">
+          {chatResponse}
+        </div>
+      </div>
+    </div>
+    
+    {/* Chat input */}
+    <div className="flex items-center p-4 border-t">
+      <input
+        type="text"
+        className="flex-1 px-4 py-2 mr-2 border rounded-full focus:outline-none focus:border-blue-500"
+        placeholder="Type a message..."
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <button className="px-4 py-2 text-white bg-blue-500 rounded-full" onClick={sendMessage}>
+        Send
+      </button>
+    </div>
+  </div>
+</div>
+
+   </>
+  );
+};
+
+export default Chat;
